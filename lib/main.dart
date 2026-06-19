50733654e2d3fbbced3a91041b256a38902b16b3
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-19 19:17:04 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,8 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     );
 
     _alignmentAnimation = Tween<Alignment>(
-      begin: const Alignment(1.9 , 3.1), 
-      end: const Alignment(0.2 , 0),
+      begin: const Alignment(2.2 , 3.2), 
+      end: const Alignment(0.2 , -0.02),
     ).animate(CurvedAnimation(
       parent: _controller,
       curve: Curves.fastOutSlowIn, 
