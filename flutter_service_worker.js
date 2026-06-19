61cd6ecaff93686443c85429f9d6c63e31cf1e19
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-19 12:18:25 +0000

    Deploying to gh-pages from @ afakhor/flutter-viewhtml@50733654e2d3fbbced3a91041b256a38902b16b3 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "index.html": "08c2bf98b8639cd3072b97a7d081dae5",
 "/": "08c2bf98b8639cd3072b97a7d081dae5",
-"main.dart.js": "18ff826e5894fab4ec7f7f6bc4ffb38b",
+"main.dart.js": "5eb914cb1d201eef769893d6f33701da",
 "version.json": "3102427ac4064654cf883da8acce5f61"};
 // The application shell files that are downloaded before a service worker can
 // start.
