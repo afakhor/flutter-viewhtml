3a8e9f62c7efe42ca6dd4102b94e2d1ae677006d
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-19 16:36:47 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
           // Background Gambar Asli (Sudah Rapi Full Layar)
           Positioned.fill(
             child: Image.asset(
-              'assets/images/bgdt.png', 
+              'assets/images/splash.png', 
               fit: BoxFit.cover,       
             ),
           ),
